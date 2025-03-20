5567b7112bcefb60971ac86d5be9d4758dab8896
Ahmad Akmal Mohd Idris <mamahopsk87@gmail.com>
2025-03-20 08:39:29 +0800

    Update URL mappings from Google Sheets

--- functions/redirect.js
+++ functions/redirect.js
@@ -1,14 +1,17 @@
-const urlMap = {
-    'jombaca': 'https://www.facebook.com/share/p/1YuEQD1KDw/',
-    'ptdi': 'https://library.uitm.edu.my/news/1842-majlis-iftar-bersama-perpustakaan-tun-dr-ismail',
-    'ppd': 'https://library.uitm.edu.my/news/1840-perbincangan-program-khidmat-komuniti-bersama-pejabat-pendidikan-daerah-segamat',
-    'ppd2': 'https://library.uitm.edu.my/news/1840-perbincangan-program-khidmat-komuniti-bersama-pejabat-pendidikan-daerah-segamat',
-    'libaturus': 'https://library.uitm.edu.my/news/1832-sesi-libat-urus-pengurusan-rekod-arkib-bersama-perpustakaan-tun-dr-ismail',
-    'bacalah': 'https://library.uitm.edu.my/budayailmu/?p=2440',
-    'ittest': 'https://johor.uitm.edu.my/index.php?option=com_sppagebuilder&view=page&id=167'
-};
 
 exports.handler = async (event) => {
+    const urlMap = {
+        'jombaca': 'https://www.facebook.com/share/p/1YuEQD1KDw/',
+        'ptdi': 'https://library.uitm.edu.my/news/1842-majlis-iftar-bersama-perpustakaan-tun-dr-ismail',
+        'ppd': 'https://library.uitm.edu.my/news/1840-perbincangan-program-khidmat-komuniti-bersama-pejabat-pendidikan-daerah-segamat',
+        'ppd2': 'https://library.uitm.edu.my/news/1840-perbincangan-program-khidmat-komuniti-bersama-pejabat-pendidikan-daerah-segamat',
+        'libaturus': 'https://library.uitm.edu.my/news/1832-sesi-libat-urus-pengurusan-rekod-arkib-bersama-perpustakaan-tun-dr-ismail',
+        'bacalah': 'https://library.uitm.edu.my/budayailmu/?p=2440',
+        'ittest': 'https://johor.uitm.edu.my/index.php?option=com_sppagebuilder&view=page&id=167',
+        'conventptdi': 'https://library.uitm.edu.my/news/1820-perbincangan-program-khidmat-komuniti-bersama-smk-canossian-convent-segamat',
+        'jadualkkb': 'https://docs.google.com/spreadsheets/d/1rOzv-9UFHyPCSE44v-cwYa29ChMto_hO9oOWNUCrP4A/edit?usp=sharing',
+    };
+
     const pathParts = event.path.split('/');
     const shortLinkId = pathParts[pathParts.length - 1]; // Get the ID from the URL
     const longUrl = urlMap[shortLinkId];
