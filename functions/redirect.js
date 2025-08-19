9961215e7a42206f9a814e9dc0dde7e185ae14e5
Ahmad Akmal Mohd Idris <mamahopsk87@gmail.com>
2025-08-19 12:21:33 +0800

    Update URL mappings from Google Sheets

--- functions/redirect.js
+++ functions/redirect.js
@@ -97,6 +97,7 @@ exports.handler = async (event) => {
         'ep1alumni': 'https://www.youtube.com/watch?v=5ANQr017Rbs',
         'teksemcee': 'https://docs.google.com/document/d/1KLJeNeaNQN8US4QITsL1WoWFhirMpDM2/edit?usp=sharing&ouid=113157731907556890038&rtpof=true&sd=true',
         'jakoafb': 'https://www.facebook.com/events/1077077431266935/?acontext=%7B%22event_action_history%22%3A[%7B%22mechanism%22%3A%22your_upcoming_events_unit%22%2C%22surface%22%3A%22bookmark%22%7D]%2C%22ref_notif_type%22%3Anull%7D',
+        'jakoayt': 'https://www.youtube.com/watch?v=vDpsw54XgLA',
     };
 
     const pathParts = event.path.split('/');
